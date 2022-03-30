a7837e66bd3b6c090dd37cb4cd46c7e7fa8367a0
tarunratan <tarunratan6@gmail.com>
2022-03-30 16:21:24 +0530

    main

--- a.java
+++ a.java
@@ -1,3 +1,26 @@
-public class a {
+public class ab
+
+{
+    public static void recr(int n)
+    {
+    int temp=1;
+    while (n!=0)
+    {
+        for(int i=0;i<=temp;i++)
+        {
+            Sysytem.out.print("*");
+        }
+        temp++;
+        System.out.println(" ");
+        n--;
+    }
+    }
+
+    public static void main(String[] args)
+    {
+        a ab= new a();
+        a.recr(5);
+        
+    }
     
 }
---
+++ ab.java
@@ -0,0 +1,11 @@
+public class ab
+
+{
+    //main method
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+
+    
+}
